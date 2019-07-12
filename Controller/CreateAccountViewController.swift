743f91f2e3bf502959885918c700fdb7de0acac9
Vishwa Shankar <vishwas1020@gmail.com>
2019-07-12 19:21:20 +0530

    Initial commit

---
+++ Controller/CreateAccountViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateAccountViewController.swift
+//  Reade
+//
+//  Created by Vishwa Shankar on 7/10/19.
+//  Copyright © 2019 Vishwa Shankar. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controller/NewsPreferencesViewController.swift
@@ -0,0 +1,11 @@
+//
+//  NewsPreferencesViewController.swift
+//  Reade
+//
+//  Created by Vishwa Shankar on 7/10/19.
+//  Copyright © 2019 Vishwa Shankar. All rights reserved.
+//
+
+import Foundation
+
+var newsTopics = ["Tech","Sports","Science","Health"]

ec37a84bd58025ee4894e45d97413a9ea8a14166
Vishwa Shankar <vishwas1020@gmail.com>
2019-08-11 23:05:53 +0400

    more UI changes for news cell
    still more work to do :)

--- Controller/HomeController.swift
+++ Controller/HomeController.swift
@@ -7,34 +7,7 @@
 //
 import UIKit
 
-class ContentCell: UICollectionViewCell{
-    
-    override init(frame:CGRect){
-        super.init(frame:frame)
-        setupViews()
-    }
-    
-    let wordLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Test test test"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    func setupViews(){
-        backgroundColor = .yellow
-        addSubview(wordLabel)
-        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
@@ -67,7 +40,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 100)
+        return CGSize(width: view.frame.width, height: 150)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
--- View/NewsCell.swift
+++ View/NewsCell.swift
@@ -11,29 +11,112 @@ import UIKit
 class NewsCell: UICollectionViewCell {
     
     
-    @IBOutlet var imageNewsCell: UIImageView!
     
-    @IBOutlet var newsTitle: UILabel!
-    @IBOutlet var newsDate: UILabel!
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
     
     public func configure(with model:NewsModel){
-        //imageNewsCell.image = model.image
         newsTitle.text = model.title
         newsDate.text = model.date
+        setupCellViews()
     }
     
-    func setupViews(){
+    let imageNewsCell: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .brown
+        return image
+    }()
+    
+    let newsTitle: UILabel = {
+        let textView = UILabel()
+        textView.text = "NEW DEVELOPER COURSE iOS"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        textView.backgroundColor = .orange
+        return textView
+    }()
+    
+    let newsDate: UILabel = {
+        let textView = UILabel()
+        textView.text = "12/12/12"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        textView.backgroundColor = .green
+        return textView
+    }()
+    
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 5
+        button.layer.borderColor = UIColor.cyan.cgColor
+        button.layer.borderWidth = 1
+        button.setTitle("Follow", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        return button
+    }()
+    
+    let sourceTextView: UILabel = {
+        let textView = UILabel()
+        textView.text = "FakeNews.io"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        textView.textColor = .black
+        textView.backgroundColor = .white
+        return textView
+    }()
+    
+    
+    func setupCellViews(){
         backgroundColor = .yellow
         addSubview(newsTitle)
-        newsTitle.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        newsTitle.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        addSubview(imageNewsCell)
+        addSubview(followButton)
+        addSubview(sourceTextView)
+        addSubview(newsDate)
+       
+        imageNewsCell.backgroundColor = .brown
+        imageNewsCell.topAnchor.constraint(equalTo: self.topAnchor, constant: 12).isActive = true
+        imageNewsCell.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12).isActive = true
+        imageNewsCell.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        imageNewsCell.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        imageNewsCell.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        sourceTextView.topAnchor.constraint(equalTo: imageNewsCell.topAnchor).isActive = true
+        sourceTextView.leftAnchor.constraint(equalTo: imageNewsCell.rightAnchor, constant: 4).isActive = true
+        sourceTextView.rightAnchor.constraint(equalTo: followButton.leftAnchor, constant: -2).isActive = true
+        sourceTextView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        sourceTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        followButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 12).isActive = true
+        followButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        followButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        followButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        followButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        newsDate.topAnchor.constraint(equalTo: sourceTextView.bottomAnchor, constant: 4).isActive = true
+        newsDate.leftAnchor.constraint(equalTo: sourceTextView.leftAnchor).isActive = true
+        newsDate.rightAnchor.constraint(equalTo: sourceTextView.rightAnchor).isActive = true
+        newsDate.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        newsDate.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        
+        newsTitle.topAnchor.constraint(equalToSystemSpacingBelow: newsDate.bottomAnchor, multiplier: 1).isActive = true
+        newsTitle.rightAnchor.constraint(equalTo: followButton.rightAnchor).isActive = true
+        newsTitle.leftAnchor.constraint(equalTo: imageNewsCell.leftAnchor, constant:  0).isActive = true
         newsTitle.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        newsTitle.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        newsTitle.translatesAutoresizingMaskIntoConstraints = false
+        
         
     }
 
